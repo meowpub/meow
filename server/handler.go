1fba6e55727ca3a1b630ea8a28d42b34c4ecb7d8
Erin Shepherd <erin.shepherd@e43.eu>
2018-05-06 18:51:19 +0100

    server: add RenderResponse utility function

--- server/handler.go
+++ server/handler.go
@@ -12,27 +12,35 @@ func WrapHandler(h api.Handler) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		ctx := req.Context()
 		resp := h(ctx, req)
-		if resp.Error != nil {
-			resp.Status = api.ErrorStatus(resp.Error)
-			resp.Data = toErrorResponse(resp.Error)
-		}
-		if resp.Data == nil {
-			return
-		}
 
-		var err error
-		switch data := resp.Data.(type) {
-		case string:
-			err = GetRender(ctx).Text(rw, resp.Status, data)
-		case []byte:
-			err = GetRender(ctx).Data(rw, resp.Status, data)
-		default:
-			err = GetRender(ctx).JSON(rw, resp.Status, data)
-		}
-
-		if err != nil {
-			rw.Write([]byte(err.Error()))
-			zap.L().Error("failed to render response", zap.Error(err))
-		}
+		RenderResponse(rw, req, resp)
 	})
+}
+
+func RenderResponse(rw http.ResponseWriter, req *http.Request, resp api.Response) {
+	ctx := req.Context()
+
+	if resp.Error != nil {
+		resp.Status = api.ErrorStatus(resp.Error)
+		resp.Data = toErrorResponse(resp.Error)
+	}
+
+	if resp.Data == nil {
+		return
+	}
+
+	var err error
+	switch data := resp.Data.(type) {
+	case string:
+		err = GetRender(ctx).Text(rw, resp.Status, data)
+	case []byte:
+		err = GetRender(ctx).Data(rw, resp.Status, data)
+	default:
+		err = GetRender(ctx).JSON(rw, resp.Status, data)
+	}
+
+	if err != nil {
+		rw.Write([]byte(err.Error()))
+		zap.L().Error("failed to render response", zap.Error(err))
+	}
 }
