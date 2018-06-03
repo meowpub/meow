257e443f7897123737efbcc5e6833d00a62c1738
Erin Shepherd <erin.shepherd@e43.eu>
2018-06-03 16:47:02 +0100

    oauth: Split Authorization from Authentication

---
+++ server/oauth/authentication.go
@@ -0,0 +1,72 @@
+package oauth
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"go.uber.org/zap"
+
+	"github.com/liclac/meow/lib"
+	"github.com/liclac/meow/models"
+	"github.com/liclac/meow/server/api"
+	"github.com/liclac/meow/server/middleware"
+)
+
+const BearerPrefix = "Bearer "
+const accessTokenKey ctxKey = "AccessToken"
+
+// Authenticated retrieves the user credentials for the request (if any)
+func Authenticate(ctx context.Context, req *http.Request) (*models.AccessToken, error) {
+	l := lib.GetLogger(ctx)
+
+	// Pull an access token from either the Authorization header (with the "Bearer " prefix),
+	// or failing that, from the "access_token" session variable (no prefix).
+	accessToken := strings.TrimPrefix(req.Header.Get("Authorization"), BearerPrefix)
+	if accessToken == "" {
+		accessToken, _ = middleware.GetSession(ctx).Values["access_token"].(string)
+	}
+	if accessToken == "" {
+		return nil, nil
+	}
+
+	// Look up the token data, reject as unauthorized if not found.
+	token, err := models.GetStores(ctx).AccessTokens().Get(accessToken)
+	if err != nil {
+		if !models.IsNotFound(err) {
+			l.Error("Access Token lookup failed", zap.Error(err), zap.String("token", accessToken))
+			return nil, err
+		}
+
+		l.Debug("Access Token is invalid/expired", zap.Error(err), zap.String("token", accessToken))
+		return nil, nil
+	}
+
+	return token, nil
+}
+
+func WithToken(ctx context.Context, tok *models.AccessToken) context.Context {
+	return context.WithValue(ctx, accessTokenKey, tok)
+}
+
+func GetToken(ctx context.Context) *models.AccessToken {
+	tok := ctx.Value(accessTokenKey)
+	if tok != nil {
+		return tok.(*models.AccessToken)
+	} else {
+		return nil
+	}
+}
+
+func AuthenticationMiddleware(next api.Handler) api.Handler {
+	return api.HandlerFunc(func(ctx context.Context, req *http.Request) api.Response {
+		tok, err := Authenticate(ctx, req)
+		if err != nil {
+			return api.ErrorResponse(err)
+		}
+
+		ctx = WithToken(ctx, tok)
+
+		return next.HandleRequest(ctx, req)
+	})
+}
--- server/oauth/authorization.go
+++ server/oauth/authorization.go
@@ -6,41 +6,21 @@ import (
 	"strings"
 
 	"github.com/pkg/errors"
-	"go.uber.org/zap"
 
 	"github.com/liclac/meow/lib"
-	"github.com/liclac/meow/models"
 	"github.com/liclac/meow/server/api"
-	"github.com/liclac/meow/server/middleware"
 )
 
-const BearerPrefix = "Bearer "
-
-// Authorize retrieves the access token from the request and ensures that it has the requested
+// Authorize retrieves the access token from the context and ensures that it has the requested
 // scope (if any). If the user is not authenticated, the returned error will have a status code
 // of 401 Unauthorized, or if the user does not have the required scope, 403 Forbidden.
-func Authorize(ctx context.Context, req *http.Request, scope *Scope) (*models.AccessToken, error) {
+func Authorize(ctx context.Context, scope *Scope) error {
 	l := lib.GetLogger(ctx)
+	token := GetToken(ctx)
 
-	// Pull an access token from either the Authorization header (with the "Bearer " prefix),
-	// or failing that, from the "access_token" session variable (no prefix).
-	accessToken := strings.TrimPrefix(req.Header.Get("Authorization"), BearerPrefix)
-	if accessToken == "" {
-		accessToken, _ = middleware.GetSession(ctx).Values["access_token"].(string)
-	}
-	if accessToken == "" {
-		return nil, api.Wrap(errors.New("authentication required"), http.StatusUnauthorized)
-	}
-
-	// Look up the token data, reject as unauthorized if not found.
-	token, err := models.GetStores(ctx).AccessTokens().Get(accessToken)
-	if err != nil {
-		if !models.IsNotFound(err) {
-			l.Error("Access Token lookup failed", zap.Error(err), zap.String("token", accessToken))
-			return nil, err
-		}
-		l.Debug("Access Token is invalid/expired", zap.Error(err), zap.String("token", accessToken))
-		return nil, api.Wrap(
+	if token == nil {
+		l.Debug("No token in context")
+		return api.Wrap(
 			errors.New("authentication required"),
 			http.StatusUnauthorized,
 		)
@@ -49,11 +29,11 @@ func Authorize(ctx context.Context, req *http.Request, scope *Scope) (*models.Ac
 	// If we're authorizing with a scope, reject as unauthorized if we don't have it.
 	scopes := strings.Split(token.Scope, " ")
 	if scope != nil && !CheckScope(scopes, scope) {
-		return nil, api.Wrap(
+		return api.Wrap(
 			errors.Errorf("this endpoint requires the %s scope, but you only have grants for %s", strings.Join(scopes, ", ")),
 			http.StatusForbidden,
 		)
 	}
 
-	return token, nil
+	return nil
 }
