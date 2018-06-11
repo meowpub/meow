c1573ea19eedd11168e8bde890dbbe0f89911b87
Ember Shepard <git@liclac.eu>
2018-06-11 01:19:21 +0100

    Adjusted /server to use the new router

--- server/login.go
+++ server/login.go
@@ -11,6 +11,12 @@ import (
 	"github.com/meowpub/meow/server/middleware"
 	"github.com/meowpub/meow/server/oauth"
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+var ErrInvalidUsernameOrPassword = api.Wrap(
+	errors.New("invalid username or password"),
+	http.StatusUnauthorized,
 )
 
 type LoginRequest struct {
@@ -18,10 +24,9 @@ type LoginRequest struct {
 	Password string `form:"password"`
 }
 
-func RenderLogin(ctx context.Context, req *http.Request) api.Response {
-	tok := oauth.GetToken(ctx)
-
-	if tok != nil {
+func HandleLogin(ctx context.Context, req *http.Request) api.Response {
+	// If the user is already logged in, redirect to their profile.
+	if tok := oauth.GetToken(ctx); tok != nil {
 		uid := tok.UserID
 		stores := models.GetStores(ctx)
 		usr, err := stores.Users().GetBySnowflake(uid)
@@ -38,53 +43,58 @@ func RenderLogin(ctx context.Context, req *http.Request) api.Response {
 		return api.RedirectResponse(usrEntity.GetID())
 	}
 
+	if req.Method == "POST" {
+		var body LoginRequest
+		if err := api.DecodeForm(req, &body); err != nil {
+			return api.Response{Error: err}
+		}
+
+		stores := models.GetStores(ctx)
+		sess := middleware.GetSession(ctx)
+
+		// Look up the user.
+		user, err := stores.Users().GetByEmail(body.Email)
+		if err != nil {
+			lib.GetLogger(ctx).Info("Login attempt with invalid email",
+				zap.String("email", body.Email))
+			return api.Response{Error: ErrInvalidUsernameOrPassword}
+		}
+
+		// Validate the password.
+		ok, err := user.CheckPassword(body.Password)
+		if err != nil {
+			lib.GetLogger(ctx).Error("Password check failed, is the password corrupt in the DB?",
+				zap.String("email", body.Email))
+			return api.Response{Error: err}
+		}
+		if !ok {
+			lib.GetLogger(ctx).Info("Login attempt with invalid password",
+				zap.String("email", body.Email))
+			return api.Response{Error: ErrInvalidUsernameOrPassword}
+		}
+
+		// Generate and store an access token.
+		token, err := lib.GenToken()
+		if err != nil {
+			return api.Response{Error: err}
+		}
+		tok := &models.AccessToken{
+			Token:    token,
+			ClientID: oauth.WebClient.GetId(),
+			Scope:    "web",
+			AuthorizationUserData: models.AuthorizationUserData{
+				UserID: user.ID,
+			},
+		}
+		if err := stores.AccessTokens().Set(tok, oauth.WebAccessTokenTTL); err != nil {
+			return api.Response{Error: err}
+		}
+
+		// Store the access token in the session.
+		sess.Values["access_token"] = token
+
+		return api.Response{Data: "OK"}
+	}
+
 	return api.Response{Template: "login"}
-}
-
-func HandleLogin(ctx context.Context, req *http.Request) api.Response {
-	var body LoginRequest
-	if err := api.DecodeForm(req, &body); err != nil {
-		return api.Response{Error: err}
-	}
-
-	stores := models.GetStores(ctx)
-	sess := middleware.GetSession(ctx)
-
-	// Look up the user.
-	user, err := stores.Users().GetByEmail(body.Email)
-	if err != nil {
-		return api.Response{Error: err}
-	}
-
-	// Validate the password.
-	ok, err := user.CheckPassword(body.Password)
-	if err != nil {
-		return api.Response{Error: err}
-	}
-	if !ok {
-		err := errors.New("invalid password")
-		return api.Response{Error: api.Wrap(err, http.StatusUnauthorized)}
-	}
-
-	// Generate and store an access token.
-	token, err := lib.GenToken()
-	if err != nil {
-		return api.Response{Error: err}
-	}
-	tok := &models.AccessToken{
-		Token:    token,
-		ClientID: oauth.WebClient.GetId(),
-		Scope:    "web",
-		AuthorizationUserData: models.AuthorizationUserData{
-			UserID: user.ID,
-		},
-	}
-	if err := stores.AccessTokens().Set(tok, oauth.WebAccessTokenTTL); err != nil {
-		return api.Response{Error: err}
-	}
-
-	// Store the access token in the session.
-	sess.Values["access_token"] = token
-
-	return api.Response{Data: "OK"}
 }
