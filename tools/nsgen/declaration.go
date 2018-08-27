90d7710d7ba92e6d489a9dde42669af8b21cb778
liclac <git@liclac.eu>
2018-08-27 12:25:20 +0100

    Use the generated code in nsgen

--- tools/nsgen/declaration.go
+++ tools/nsgen/declaration.go
@@ -4,20 +4,8 @@ import (
 	"strings"
 
 	"github.com/meowpub/meow/ld"
-)
-
-const (
-	RDF      = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-	RDF_type = RDF + "type"
-
-	RDFS             = "http://www.w3.org/2000/01/rdf-schema#"
-	RDFS_comment     = RDFS + "comment"
-	RDFS_isDefinedBy = RDFS + "isDefinedBy"
-	RDFS_label       = RDFS + "label"
-	RDFS_subClassOf  = RDFS + "subClassOf"
-	RDFS_seeAlso     = RDFS + "seeAlso"
-	RDFS_domain      = RDFS + "domain"
-	RDFS_range       = RDFS + "range"
+	"github.com/meowpub/meow/ld/ns/rdf"
+	"github.com/meowpub/meow/ld/ns/rdfs"
 )
 
 var _ ld.Entity = Declaration{}
@@ -43,9 +31,9 @@ func (t Declaration) TypeName() string {
 }
 
 func (t Declaration) RDFType() string {
-	return ld.ToObject(t.V[RDF_type]).ID()
+	return ld.ToObject(t.V[rdf.PropType]).ID()
 }
 
 func (t Declaration) Domain() string {
-	return ld.ToObject(t.V[RDFS_domain]).ID()
+	return ld.ToObject(t.V[rdfs.PropDomain]).ID()
 }
