6805937ffc6eeac917dc146b2dd47df46c6a875b
embr <git@liclac.eu>
2018-10-27 23:41:53 +0100

    dropped more old code

--- ns/ns.go
+++
@@ -1,9 +0,0 @@
-package ns
-
-func AS(postfix string) string {
-	return "https://www.w3.org/ns/activitystreams#" + postfix
-}
-
-func LDP(postfix string) string {
-	return "http://www.w3.org/ns/ldp#" + postfix
-}
