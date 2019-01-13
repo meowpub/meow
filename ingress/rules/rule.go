afcba517e3ed52fd13ce51d5019f41784e4fb45b
embr <hi@liclac.eu>
2019-01-13 20:17:56 +0000

    Run batches of rules

--- ingress/rules/rule.go
+++ ingress/rules/rule.go
@@ -14,8 +14,18 @@ var Rules = []Rule{
 	C2SRequiredActivityProperties, // Check required properties.
 	C2SGenerateIDs,                // Generate activity/object IDs, discarding old ones.
 
-	RequireActivity,
+	RequireActivity, // Require input to be a proper Activity.
 }
 
 // A rule can process or reject an activity before it's ingested.
 type Rule func(ctx context.Context, src ld.Source, actor, stream *models.Entity, activity *as.Activity) error
+
+// Runs a series of rules on the input.
+func Run(ctx context.Context, rules []Rule, src ld.Source, actor, stream *models.Entity, activity *as.Activity) error {
+	for _, rule := range rules {
+		if err := rule(ctx, src, actor, stream, activity); err != nil {
+			return err
+		}
+	}
+	return nil
+}
