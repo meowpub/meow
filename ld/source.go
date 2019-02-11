c9a06d4d25783fb68e43a99fb8d3a79751c47994
embr <hi@liclac.eu>
2019-02-11 02:00:05 +0000

    ld.Source.String()

--- ld/source.go
+++ ld/source.go
@@ -1,8 +1,23 @@
 package ld
 
+import (
+	"fmt"
+)
+
 type Source int
 
 const (
 	ClientToServer Source = iota
 	ServerToServer
 )
+
+func (s Source) String() string {
+	switch s {
+	case ClientToServer:
+		return "ClientToServer"
+	case ServerToServer:
+		return "ServerToServer"
+	default:
+		return fmt.Sprintf("[%d]", s)
+	}
+}
